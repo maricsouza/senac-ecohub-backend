88acb8ca40c66865b292664450e48768dd563bfb
Raian Medeiros <raiannolaco@gmail.com>
2024-05-27 01:38:35 -0300

    Add SQL

--- ecohub/Ecohub/Ecohub/obj/Debug/net8.0/Ecohub.AssemblyInfo.cs
+++ ecohub/Ecohub/Ecohub/obj/Debug/net8.0/Ecohub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecohub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aceb5b703f40d26aebb8d68c4c08e962e79f534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06513bf8af858eef79ec2f1cea1e7da5ea5b196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecohub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecohub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
